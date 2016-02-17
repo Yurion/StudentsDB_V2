63b8e9408790fe0d8cfea43658eb41d5ad20322e
Yuriy Khomyk <khomykyu@gmail.com>
2016-02-17 23:57:57 +0200

    write students_list view, add url to urls.py

--- students/views.py
+++ students/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from django.template import RequestContext, loader
 
-# Create your views here.
+def students_list(request):
+    return HttpResponse('<h1>Hello World</h1>')
+
+def students_list2(request, sid):
+    try:
+        sid = int(sid)
+    except ValueError:
+        raise Http404
+    else:
+        return HttpResponse('<h1>Hello World</h1>')
+
+def students_list3(request):
+    template = loader.get_template('demo.html')
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
+
+def students_list4(request):
+    return render(request, 'demo.html', {})
