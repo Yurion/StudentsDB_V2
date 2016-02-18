8d4d5240c7fa6a7c0b49536213d491fd348f84e8
Yuriy Khomyk <khomykyu@gmail.com>
2016-02-18 17:34:59 +0200

    add students_list.html

--- students/views.py
+++ students/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.template import RequestContext, loader
 
+
 def students_list(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'students/students_list.html', {})
+
 
 def students_list2(request, sid):
     try:
@@ -14,10 +16,12 @@ def students_list2(request, sid):
     else:
         return HttpResponse('<h1>Hello World</h1>')
 
+
 def students_list3(request):
     template = loader.get_template('demo.html')
     context = RequestContext(request, {})
     return HttpResponse(template.render(context))
+
 
 def students_list4(request):
     return render(request, 'demo.html', {})
